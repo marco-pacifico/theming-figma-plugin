e5d013a31293e36aa5ba1cfe9167b7a9b319a43d
Marco Pacifico <marco.pacifico@gmail.com>
2024-06-01 19:22:19 -0400

    swap text styles

--- src/lib/swap-text-styles.ts
+++ src/lib/swap-text-styles.ts
@@ -1,52 +1,29 @@
-import { getNode } from "./utils/nodes";
-import { getVariablesInCollection } from "./utils/variables";
-
 export default async function swapTextStyles() {
+  // Get text nodes in the file
+  const allTextNodesInFile = figma.currentPage.findAll(
+    (n) => n.type === "TEXT"
+  ) as TextNode[];
 
-  // Create style
-  const textStyle = figma.createTextStyle();
-  const textStyleId = textStyle.id;
-  textStyle.name = "Test Text Style";
-
-  // Create text node
-  const text = figma.createText();
-  text.characters = "Test Text Node";
-  text.setTextStyleIdAsync(textStyle.id);
-
-  const typographyVariables = await getVariablesInCollection("_Typography");
-  const headingFontFamily = typographyVariables.find(
-    (variable) => variable?.name === "heading-font-family"
-  );
-  const headingFontStyle = typographyVariables.find(
-    (variable) => variable?.name === "heading-font-style"
+  // Get all local heading text styles
+  const headingTextStyles = (await figma.getLocalTextStylesAsync()).filter(
+    (textStyle) => textStyle.name.includes("Heading")
   );
 
-  // Apply variables
-  if (headingFontFamily && typeof headingFontFamily !== "undefined") {
-    textStyle.setBoundVariable("fontFamily", headingFontFamily);
-  }
-  if (headingFontStyle && typeof headingFontStyle !== "undefined") {
-    textStyle.setBoundVariable("fontStyle", headingFontStyle);
-  }
-  
 
-  // Getting a text node on the page that's using a library text style
-  // Text node is in a frame called Heading
-  const TestingInstance = getNode({
-    name: "Testing",
-    type: "INSTANCE",
-    parent: figma.currentPage,
-  });
-  if (TestingInstance) {
-    const libraryTextNode = getNode({
-      name: "Heading/5XL (Desktop)",
-      type: "TEXT",
-      parent: TestingInstance as InstanceNode,
-    }) as TextNode;
+  // Swap component instance text styles with local text styles
+  // Match text styles by name
+  for (const textNode of allTextNodesInFile) {
+    // Get the name of the text style attached to the text node
+    const textStyleId = textNode.textStyleId;
+    const textStyle =
+      textStyleId !== figma.mixed &&
+      (await figma.getStyleByIdAsync(textStyleId));
+    const textStyleName = textStyle && textStyle.name;
 
-    // Set style on library text node to the first local text style
-    if (typeof textStyleId === "string") {
-      await libraryTextNode.setTextStyleIdAsync(textStyleId);
+    for (const headingTextStyle of headingTextStyles) {
+      if (textStyleName === headingTextStyle.name) {
+        await textNode.setTextStyleIdAsync(headingTextStyle.id);
+      }
     }
   }
 }
